c9e207247ef6cf66a248a29aefe8cdb3ac1d8c4e
Qi Ge <4011632+GeQi@users.noreply.github.com>
2023-11-28 00:59:11 -0700

    Update Basic.lean

--- Project/Basic.lean
+++ Project/Basic.lean
@@ -1,16 +1,44 @@
 import Mathlib.Algebra.Category.Ring.Basic
 import Mathlib.CategoryTheory.ConcreteCategory.Bundled
 import Mathlib.GroupTheory.GroupAction.Defs
+import Mathlib.Topology.Basic
+import Mathlib.Topology.Algebra.MulAction
+import Mathlib.Topology.Algebra.Monoid
+import Mathlib.Topology.Algebra.Ring.Basic
 
-class Module' (R : Type*) [Semiring R] (M : Type*) where
-  isAddCommMonoid : AddCommMonoid M
-  isDistribMulAction : DistribMulAction R M
-  add_smul : ∀ (r s : R) (x : M), (r + s) • x = r • x + s • x
-  zero_smul : ∀ x : M, (0 : R) • x = 0
+class MulActionSemiModule (G R M : Type*)
+    [Monoid G]
+      [Semiring R] [MulSemiringAction G R]
+        [AddCommMonoid M] [Module R M] [MulAction G M]
+          where
+  comp: ∀ g : G, ∀ r : R, ∀ m : M, g • (r • m) = (g • r) • (g • m)
 
-open CategoryTheory
-def ModuleCat' (R : Type u) [Semiring R] := Bundled (Module' R)
+class MulActionSemiModuleHom (G R R' M M': Type*)
+    [Monoid G]
+      [Semiring R] [MulSemiringAction G R]
+        [Semiring R'] [MulSemiringAction G R']
+          [AddCommMonoid M] [Module R M] [MulAction G M] [MulActionSemiModule G R M]
+            [AddCommMonoid M'] [Module R' M'] [MulAction G M'] [MulActionSemiModule G R' M']
+              where
+  -- comp: ∀ g : G, ∀ r : R, ∀ m : M, g • (r • m) = (g • r) • (g • m)
 
-#check Module
-variable (R : Type u) [Semiring R]
-#check Module R
+class ContinuousMulAction (M S : Type*)
+  [Monoid M] [TopologicalSpace M] [ContinuousMul M] [TopologicalSpace S]
+    extends MulAction M S, ContinuousSMul M S where
+
+class ContinuousMulSemiringAction (M R : Type*)
+  [Monoid M] [TopologicalSpace M] [ContinuousMul M] [TopologicalSpace R] [Semiring R] [TopologicalSemiring R]
+    extends MulAction M R, ContinuousSMul M R where
+
+-- class Module' (R : Type*) [Semiring R] (M : Type*) where
+--   isAddCommMonoid : AddCommMonoid M
+--   isDistribMulAction : DistribMulAction R M
+--   add_smul : ∀ (r s : R) (x : M), (r + s) • x = r • x + s • x
+--   zero_smul : ∀ x : M, (0 : R) • x = 0
+
+-- open CategoryTheory
+-- def ModuleCat' (R : Type u) [Semiring R] := Bundled (Module' R)
+
+-- #check Module
+-- variable (R : Type u) [Semiring R]
+-- #check Module R
